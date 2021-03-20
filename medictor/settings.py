c811e41e1a76bd6228f48d075fe4df132662de5a
mr-sky8214 <67673321+mr-sky8214@users.noreply.github.com>
2021-03-20 17:37:02 +0530

    Update settings.py

--- medictor/settings.py
+++ medictor/settings.py
@@ -133,5 +133,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'medicatorvs@gmail.com'
-EMAIL_HOST_PASSWORD = 'vishivsky'
